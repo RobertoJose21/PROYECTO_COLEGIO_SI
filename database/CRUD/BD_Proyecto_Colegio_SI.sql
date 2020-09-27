2048084bc2df20b32476309214a865158a31b51d
leodan12 <leomacip4@gmail.com>
2020-09-26 21:32:28 -0500

    ultimo

--- database/CRUD/BD_Proyecto_Colegio_SI.sql
+++ database/CRUD/BD_Proyecto_Colegio_SI.sql
@@ -225,17 +225,16 @@ CREATE TABLE cursos
 	curso                VARCHAR(50) NOT NULL,
 	estado               CHAR(1) NOT NULL,
 	codigocurso          CHAR(2) NOT NULL,
-	estado				 CHAR(1) NOT NULL,
 	PRIMARY KEY (idcurso),
 	FOREIGN KEY (idgrado) REFERENCES grados(idgrado)
 );
 
-insert into cursos values('1',NULL,'MATEMATICA','MA','1');
-insert into cursos values('1',NULL,'COMUNICACION','CO','1');
-insert into cursos values('2',NULL,'MATEMATICA','MA','1');
-insert into cursos values('2',NULL,'COMUNICACION','CO','1');
-insert into cursos values('2',NULL,'CIENCIA Y AMBIENTE','CA','1');
-insert into cursos values('3',NULL,'ARTE','AR','1');
+insert into cursos values('1',NULL,'MATEMATICA','1','MA');
+insert into cursos values('1',NULL,'COMUNICACION','1','CO');
+insert into cursos values('2',NULL,'MATEMATICA','1','MA');
+insert into cursos values('2',NULL,'COMUNICACION','1','CO');
+insert into cursos values('2',NULL,'CIENCIA Y AMBIENTE','1','CA');
+insert into cursos values('3',NULL,'ARTE','1','AR');
 
 CREATE TABLE capacidades
 (
@@ -290,7 +289,7 @@ CREATE TABLE detalle_catedra
 	idprofesor           INT NOT NULL,
 	estado               CHAR(1) NOT NULL,
 	PRIMARY KEY (idcurso,idprofesor),
-	FOREIGN KEY (idcurso) REFERENCES (idcurso),
+	FOREIGN KEY (idcurso) REFERENCES cursos(idcurso),
     FOREIGN KEY (idprofesor) REFERENCES profesores(idprofesor)
 );
  
