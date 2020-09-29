9b97503087356ceed3dafd9dbcd897d314dd78cd
RobertoJose21 <rvaldivia@unitru.edu.pe>
2020-09-29 11:27:15 -0500

    reverti lo del dompdf

--- config/app.php
+++ config/app.php
@@ -139,7 +139,6 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Barryvdh\DomPDF\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -190,7 +189,7 @@ return [
     */
 
     'aliases' => [
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
